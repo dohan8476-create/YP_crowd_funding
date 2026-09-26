338213b1a7ddd29c8652901dff89f4373ba994e2
dohankim <dohan230118@gmail.com>
2026-09-26 22:32:16 +0900

    wip: Redesign FUNDS and REWARDS table structures - PK 변경 및 참조 변경 설계의 대한 논의 필요

--- src/main/java/kr/yp_crowdfunding/persistence/dto/FundDTO.java
+++ src/main/java/kr/yp_crowdfunding/persistence/dto/FundDTO.java
@@ -10,6 +10,10 @@ import java.util.Date;
 @Setter
 @ToString
 public class FundDTO {
+    //memo: 이거 amount 빠진듯
+    //reward 테이블과의 연결성이 낮아보임
+    //실질적으로 fund랑 reward가 서로서로 잘 연동되어 계산되어야할 거 같은데
+    //이거도 회의 ㄱㄱ
     private Long userID;
     private Long projectID;
     private String rewardName;
--- src/main/java/kr/yp_crowdfunding/persistence/dto/RewardDTO.java
+++ src/main/java/kr/yp_crowdfunding/persistence/dto/RewardDTO.java
@@ -8,6 +8,11 @@ import lombok.ToString;
 @Setter
 @ToString
 public class RewardDTO {
+    //얘도 찾아보니까 name이 projectId랑 묶여있으면
+    //리워드 오타수정(update)가 발생하면 참조 무결성 위반 및 대규모 cascade 발생
+    //심하면 변경 이전의 name의 data들이 그냥 방치되어있을 수도 있음
+    //단일pk로 rewardId 만들 필요 성의 대한 회의 필요
+    //그리고 중복 불가를 pk말고 그냥 unique 속성 활용으로 고려해도 될듯?
     private Long projectID;
     private String name;
     private String description;
